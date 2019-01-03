ababc915f221c1d739e979966282a795c2148050
asus <asus@MICROSOFT-PROGR>
2019-01-03 16:31:59 +0330

    Searhc and Province Ajax

--- Profile/Controllers/UserController.cs
+++ Profile/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Profile.Models;
 using Profile.Models.Entities;
 
@@ -17,9 +18,47 @@ namespace Profile.Controllers
             _context = context;
         }
 
-        public IActionResult UserList()
+        public IActionResult UserList(string searchFullname = "", string sortAge = "", string sortName = "")
         {
-            return View(_context.Users.ToList());
+            IEnumerable<User> model;
+
+            if (string.IsNullOrEmpty(searchFullname))
+            {
+                model = _context.Users;
+            }
+            else
+            {
+                model = _context.Users
+                    .Where(u => u.FirstName.Contains(searchFullname) || u.LastName.Contains(searchFullname));
+            }
+
+            switch (sortAge)
+            {
+                case "None":
+                    model = _context.Users;
+                    break;
+
+                case "Up":
+                    model = _context.Users.OrderBy(u => u.Age);
+                    break;
+
+                case "Down":
+                    model = _context.Users.OrderByDescending(u => u.Age);
+                    break;
+            }
+
+            if (string.IsNullOrEmpty(sortName))
+            {
+                model = _context.Users;
+            }
+            else
+            {
+                model = _context.Users.OrderBy(u => u.FirstName);
+            }
+
+            ViewData["Search"] = searchFullname;
+            ViewData["Sort"] = sortAge;
+            return View(model);
         }
 
         public IActionResult CreateUser()
@@ -88,6 +127,30 @@ namespace Profile.Controllers
         {
             var findUser = _context.Users.Find(id);
             return View(findUser);
+        }
+
+        public IActionResult CityAjax(string province)
+        {
+            string model = "";
+
+            switch (province)
+            {
+                case "Golestan":
+                    model = "Gorgan";
+                    break;
+
+                case "Mazandaran":
+                    model = "Sari";
+                    break;
+
+                case "Gilan":
+                    model = "Lahijan";
+                    break;
+
+                default:
+                    break;
+            }
+            return Json(model);
         }
     }
 }
